e8c1e986fd33c5c5b9c37bcadfe85659fe8632e0
songawee <dennis@songawee.com>
2013-12-28 10:15:29 -0700

    Add selection sort

--- js/selection.js
+++ js/selection.js
@@ -2,3 +2,23 @@
 
 var array = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 
+function sort(array) {
+  var min = 0;
+  var swap = 0;
+  for (var i = 0; i < array.length; i++) {
+    // find min
+    min = i;
+    for (var j = i; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    // swap
+    swap = array[i];
+    array[i] = array[min];
+    array[min] = swap;
+  }
+}
+
+sort(array);
+console.log(array);
